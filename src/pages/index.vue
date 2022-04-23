b07ae2b29e5236efcfc496261f6a70207e81a5c7
Amon Sawamura <amotarao@gmail.com>
2022-04-23 20:12:05 +0900

    add buttons

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,22 @@
 <script lang="ts" setup>
 const count = ref<number>(0);
 
-const increment = () => {
-  count.value++;
+const increment = (value: number = 1): void => {
+  count.value += value;
 };
 
-const decrement = () => {
-  count.value--;
+const decrement = (value: number = 1): void => {
+  count.value -= value;
 };
 </script>
 
 <template>
-  <h1>Hello World!</h1>
-  <p>Count: {{ count }}</p>
-  <button @click="increment">+</button>
-  <button @click="decrement">-</button>
+  <h1 class="text-2xl">Hello World!</h1>
+  <p class="mt-2">Count: {{ count }}</p>
+  <div class="flex gap-2 mt-2">
+    <button class="px-2 py-1 bg-slate-900 text-white font-mono" @click="decrement(10)">--</button>
+    <button class="px-2 py-1 bg-slate-900 text-white font-mono" @click="decrement()">-</button>
+    <button class="px-2 py-1 bg-slate-900 text-white font-mono" @click="increment()">+</button>
+    <button class="px-2 py-1 bg-slate-900 text-white font-mono" @click="increment(10)">++</button>
+  </div>
 </template>
