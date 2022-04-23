4a520b826451a4604ecd0d0f92ba50190aac0eeb
Amon Sawamura <amotarao@gmail.com>
2022-04-23 20:20:23 +0900

    baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,6 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   srcDir: 'src',
+  baseURL: process.env.BASE_URL || '/',
   modules: ['@nuxtjs/tailwindcss'],
 });
