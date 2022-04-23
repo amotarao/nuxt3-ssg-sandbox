480febebed39e620ec4714bec14264530b6a433f
Amon Sawamura <amotarao@gmail.com>
2022-04-23 20:33:08 +0900

    remove baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,5 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   srcDir: 'src',
-  baseURL: process.env.BASE_URL || '/',
   modules: ['@nuxtjs/tailwindcss'],
 });
