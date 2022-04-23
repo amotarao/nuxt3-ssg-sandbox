cdedb98c862b020d036036628ad4f40b6582e170
Amon Sawamura <amotarao@gmail.com>
2022-04-23 20:57:14 +0900

    add styles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,12 +11,14 @@ const decrement = (value: number = 1): void => {
 </script>
 
 <template>
-  <h1 class="text-2xl">Hello World!</h1>
-  <p class="mt-2">Count: {{ count }}</p>
-  <div class="flex gap-2 mt-2">
-    <button class="px-2 py-1 bg-slate-900 text-white font-mono" @click="decrement(10)">--</button>
-    <button class="px-2 py-1 bg-slate-900 text-white font-mono" @click="decrement()">-</button>
-    <button class="px-2 py-1 bg-slate-900 text-white font-mono" @click="increment()">+</button>
-    <button class="px-2 py-1 bg-slate-900 text-white font-mono" @click="increment(10)">++</button>
+  <div class="p-4 flex flex-col items-center gap-2">
+    <h1 class="text-2xl">Hello World!</h1>
+    <p class="">Count: {{ count }}</p>
+    <div class="flex gap-2">
+      <button class="px-4 py-2 bg-slate-900 text-white font-mono" @click="decrement(10)">--</button>
+      <button class="px-4 py-2 bg-slate-900 text-white font-mono" @click="decrement()">-</button>
+      <button class="px-4 py-2 bg-slate-900 text-white font-mono" @click="increment()">+</button>
+      <button class="px-4 py-2 bg-slate-900 text-white font-mono" @click="increment(10)">++</button>
+    </div>
   </div>
 </template>
