9eb82403aeae7a70f44ee07d869272adfe45fb0d
Amon Sawamura <amotarao@gmail.com>
2022-04-23 19:54:51 +0900

    count

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script lang="ts" setup>
+const count = ref<number>(0);
+
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  count.value--;
+};
+</script>
 
 <template>
   <h1>Hello World!</h1>
+  <p>Count: {{ count }}</p>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
 </template>
