0811804a98988d8f520fa5e438c227aa5e04c74f
Amon Sawamura <amotarao@gmail.com>
2022-04-23 21:19:13 +0900

    add pages

---
+++ src/pages/blog/[slug].vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const route = useRoute();
+
+const { data, pending } = useAsyncData(
+  'blog',
+  async () => {
+    return {
+      title: route.params.slug as string,
+    };
+  },
+  {
+    server: true,
+  }
+);
+</script>
+
+<template>
+  <div class="p-4 flex flex-col gap-2">
+    <p v-if="pending">読み込み中</p>
+    <div v-else>
+      <h1 class="text-2xl">{{ data.title }}</h1>
+    </div>
+  </div>
+</template>
